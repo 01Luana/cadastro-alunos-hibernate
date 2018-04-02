192656c3c4218840e3c9c354b617f9cdd4efc8e1
04381210239 <04381210239@JIPA-LABIN05-11.ifro.local>
2018-04-02 09:12:27 -0400

    Iniciando a instalação de hibernate

---
+++ CadastroAlunoHibernate/src/br/edu/ifro/medelo/Aluno.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifro.medelo;
+import javax.persistece.Id;
+
+
+@Entity
+public class Aluno {
+    @Id;
+    @GeneretedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Integer idade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+    
+}
